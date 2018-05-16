9a9acd4443c8be9d34d000c260a7e1b817750ebb
Daniel Howard <danielhoward314@gmail.com>
2018-05-16 12:54:45 -0400

    finishes terminal version of game with no front end

--- game/index.js
+++ game/index.js
@@ -1,6 +1,27 @@
-import {Map} from 'immutable'
+import {Map} from 'immutable';
+import { createStore } from 'redux';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const MOVE = 'MOVE';
+
+let board = Map();
+
+export const move = (player, coords) => {
+  return {type: MOVE, coords, player};
+};
+
+// const store = createStore(reducer);
+
+export default function reducer(state = {turn: 'X', board: board}, action) {
+  switch (action.type) {
+    case MOVE:
+    if (state.turn === 'X') {
+      return { turn: 'O', board: state.board.setIn(action.coords, action.player) };
+    } else {
+        return { turn: 'X', board: state.board.setIn(action.coords, action.player) };
+    }
+    default:
+    return state;
+  }
 }
+
+
